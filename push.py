aa828c2b4ac0475a0e0a9247e42ce0d8035ee939
Esther Huang <yayadashone@gmail.com>
2025-06-17 15:52:35 +0800

    Update push.py

--- push.py
+++ push.py
@@ -1,21 +1,12 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from google_sheet import get_all_user_ids
 import os
-import requests
-from dotenv import load_dotenv
 
-load_dotenv()
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 
-LINE_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-
-headers = {
-    "Authorization": f"Bearer {LINE_TOKEN}",
-    "Content-Type": "application/json"
-}
-
-def send_push(user_ids, message):
-    for i in range(0, len(user_ids), 500):
-        data = {
-            "to": user_ids[i:i+500],
-            "messages": [{"type": "text", "text": message}]
-        }
-        r = requests.post("https://api.line.me/v2/bot/message/multicast", json=data, headers=headers)
-        print(r.status_code, r.text)
+def send_push(message):
+    user_ids = get_all_user_ids()
+    chunks = [user_ids[i:i + 150] for i in range(0, len(user_ids), 150)]
+    for chunk in chunks:
+        line_bot_api.multicast(chunk, TextSendMessage(text=message))
