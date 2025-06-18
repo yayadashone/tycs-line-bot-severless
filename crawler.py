ecfc8b55e00095cad91557eee0927fbbcc5d245a
estherhuang <yayadashone@gmail.com>
2025-06-18 12:35:42 +0800

    imstall package

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 import time
 
 options = Options()
