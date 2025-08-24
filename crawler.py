e8b8e8280bbbc89e3eb2a4d87a61b18afd6a7afd
estherhuang <yayadashone@gmail.com>
2025-08-24 18:40:00 +0800

    fix event_url get_attr()

--- crawler.py
+++ crawler.py
@@ -36,7 +36,7 @@ def crawl_events(pages=5):
                 date_apply = event.find_element(By.CLASS_NAME, 'date-apply').text.strip().replace('\n', ' ')
                 date_cancel = event.find_element(By.CLASS_NAME, 'date-cancel').text.strip()
 
-                event_url = event.find_element(By.CLASS_NAME, 'event-info').get_attribute('href').strip()
+                event_url = event.find_element(By.CLASS_NAME, 'event-info').get_attribute('href')
                 print(f"活動名稱: {name}, 活動日期: {date_event}, 報名日期: {date_apply}, 取消日期: {date_cancel}, 活動網址: {event_url}")
 
 
@@ -47,7 +47,6 @@ def crawl_events(pages=5):
                     "date_event": date_event,
                     "date_apply": date_apply,
                     "date_cancel": date_cancel,
-
                     "event_url": event_url
 
                 }
